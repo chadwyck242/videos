c7493ceb0b415fb3617348c9574f85af7e71f98a
chadwyck242 <chadwyck.codeart@gmail.com>
2019-01-09 20:06:42 -0700

    Add state object to SearchBar component, create input event handlers

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,18 +1,22 @@
-import React from 'react';
+import React from "react";
 
 class SearchBar extends React.Component {
+  state = { term: "" };
+
   render() {
     return (
-      <div className="search-bar ui segment">
+      <div className="search-bar ui segment inverted">
         <form className="ui form">
           <div className="field">
-            <label>Video Search</label>
-            <input type="text"/>
+            <label className="ui medium top left attached label teal">
+              Video Search
+            </label>
+            <input type="text" value={this.state.term} onChange={this.onInputChange} />
           </div>
         </form>
       </div>
     );
   }
-};
+}
 
 export default SearchBar;

67eec8dac80bf8e4e58ba65a56f39dbce6e18cd6
chadwyck242 <chadwyck.codeart@gmail.com>
2019-01-10 20:41:43 -0700

    Install axios and configure YouTube API instance

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,12 @@
+import axios from 'axios';
+
 const KEY = 'AIzaSyB5txLZnIiFDJ1KR1xmpLxApqto92vT3Ic';
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    maxResults: 5,
+    key: KEY  
+  }
+});

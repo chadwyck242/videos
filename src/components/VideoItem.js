3d3584ae0b8a76eedc335d9cf0bdcd8426ca6f7b
Chad Sisk <chadwyck.codeart@gmail.com>
2019-01-15 18:07:29 -0700

    Add Semantic UI List styling with image and header classes

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -2,9 +2,17 @@ import React from "react";
 
 const VideoItem = ({ video }) => {
   return (
-    <div>
-    <img src={video.snippet.thumbnails.medium.url} alt={video.snippet.description} />
-      {video.snippet.title}
+    <div className="item">
+      <img
+        className="ui image"
+        src={video.snippet.thumbnails.medium.url}
+        alt={video.snippet.description}
+      />
+      <div className="content">
+        <div className="header">
+          {video.snippet.title}
+        </div>
+      </div>
     </div>
   );
 };

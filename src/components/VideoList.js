6a010f64c2406f7c0a4940d6643e792f50af6b0e
Chad Sisk <chadwyck.codeart@gmail.com>
2019-01-15 17:34:24 -0700

    Create title and thumbnails for each video and create list in VideoItem.js

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -2,11 +2,11 @@ import React from "react";
 import VideoItem from "./VideoItem";
 
 const VideoList = ({ videos }) => {
-    const renderedList = videos.map((video) => {
-        return <VideoItem />;
-    });
+  const renderedList = videos.map(video => {
+    return <VideoItem video={video} />;
+  });
 
-    return <div>{renderedList}</div>
+  return <div>{renderedList}</div>;
 };
 
 export default VideoList;

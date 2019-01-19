d4ee4148af7994921000e2730f0e93fe47ed23a3
Chad Sisk <chadwyck.codeart@gmail.com>
2019-01-18 20:48:30 -0700

    Add Semantic UI styling to VideoDetail component with Segment and Header

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const VideoDetail = ({ video }) => {
-    return <div>{video.snippet.title}</div>
+    if (!video) {
+        return <div>Loading...</div>
+    }
+
+    return (
+        <div>
+            <div className="ui segment">
+                <h4 className="ui header">{video.snippet.title}</h4>
+                <p>{video.snippet.description}</p>
+            </div>
+        </div>
+    );
 };
 
 export default VideoDetail;

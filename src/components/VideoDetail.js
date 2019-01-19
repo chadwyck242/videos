7b67ac5300b4d62cdf84e39100090f392d6e7c2d
Chad Sisk <chadwyck.codeart@gmail.com>
2019-01-18 21:25:10 -0700

    Add key to videos map function in VideoList.js as required by JSX

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,23 +1,27 @@
 import React from "react";
 
 const VideoDetail = ({ video }) => {
-    if (!video) {
-        return <div>Loading...</div>
-    }
+  if (!video) {
+    return <div>Loading...</div>;
+  }
 
-    const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+  const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
 
-    return (
-        <div>
-            <div className="ui embed">
-                <iframe data-source="youtube" src={videoSrc} title={video.snippet.title} />
-            </div>
-            <div className="ui segment">
-                <h4 className="ui header">{video.snippet.title}</h4>
-                <p>{video.snippet.description}</p>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div className="ui embed">
+        <iframe
+          data-source="youtube"
+          src={videoSrc}
+          title={video.snippet.title}
+        />
+      </div>
+      <div className="ui segment">
+        <h4 className="ui header">{video.snippet.title}</h4>
+        <p>{video.snippet.description}</p>
+      </div>
+    </div>
+  );
 };
 
 export default VideoDetail;
